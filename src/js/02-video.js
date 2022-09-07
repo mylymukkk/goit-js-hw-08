a36110d783437000675bb90da5350587085f070b
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2022-09-08 01:58:58 +0300

    finished 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const updateTime = function (timeupdate) {
+  const currentTime = timeupdate.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+  console.log(currentTime);
+};
+
+player.on('timeupdate', throttle(updateTime, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    console.log('The actual time that the player seeked to: ', seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log(
+          'Error: the time was less than 0 or greater than the video’s duration'
+        );
+        break;
+      default:
+        console.log('Error');
+        break;
+    }
+  });
