363839526311fb35b2e2174032bff39064feb3e7
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2022-09-08 14:27:49 +0300

    finished 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
+const form = document.querySelector('form');
+
+const feedbackFormState = { email: '', message: '' };
+const feedbackFormStateParse = JSON.parse(
+  localStorage.getItem('feedback-form-state')
+);
+
+fillFields();
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', handleSubmit);
+
+function fillFields() {
+  if (!localStorage.getItem('feedback-form-state')) {
+    return;
+  }
+  const { email, message } = feedbackFormStateParse;
+  email ? (emailInput.value = email) : (emailInput.value = '');
+  message ? (messageInput.value = message) : (messageInput.value = '');
+}
+
+function onInput(event) {
+  feedbackFormState[event.target.name] = event.target.value;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(`Feedback form state: `, feedbackFormState);
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
